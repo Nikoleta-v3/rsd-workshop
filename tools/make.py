31c598ab4fbfcdb81be9fa33a072614e41d17c87
Carsten Fortmann-Grote <grotec@.evolbio.mpg.de>
2021-01-14 09:44:20 +0100

    Update make.py
    Use pandoc to convert notebooks to pdf.

--- tools/make.py
+++ tools/make.py
@@ -1,19 +1,50 @@
 import glob
 import subprocess
+import shutil
+import shlex
+import logging
+import sys
+logging.basicConfig(level=logging.INFO)
+
 import os
 
 ipynbs = glob.glob("*ipynb")
+if len(ipynbs) < 1:
+    logging.warning("No notebooks found.")
+    sys.exit(1)
+else:
+    logging.info("Found %d notebooks.", len(ipynbs))
 
 
 for ipynb in ipynbs:
-    tex = ipynb[:-6] + ".tex"
-    subprocess.call(["jupyter-nbconvert", "--to", "latex", ipynb])
-    subprocess.call(["latexmk", "--xelatex", tex])
-    subprocess.call(["latexmk", "-c"])
+    logging.info("Processing %s.", ipynb)
+    # Get pure filename, no parend dirs.
+    pdf = os.path.basename(ipynb)
+    # Strip extension
+    pdf = ".".join(pdf.split(".")[:-1])
+    
+    # Append pdf extension
+    pdf += ".pdf"
 
-subprocess.call("mv *.tex Material\ in\ pdf/", shell=True)
-subprocess.call("mv *.pdf Material\ in\ pdf/", shell=True)
+    # Prepend directory for output.
+    pdf = os.path.join("Material in pdf", pdf)
 
-extensions = ["xdv", "out", "fls", "fdb_latexmk", "aux", "log"]
-for extension in extensions:
-    subprocess.call(f"rm *{extension}", shell=True)
+    logging.info("\tWill save generated pdf to %s.", pdf)
+    
+    # Setup the pandoc command.
+    command = 'pandoc -t pdf -f ipynb -o "{}" "{}"'.format(pdf, ipynb)
+    logging.info("Assembled pandoc command: %s.", command)
+    
+    # Split into tokens.
+    cmd_list = shlex.split(command)
+
+    # Run as subprocess, use pipe to gather stdin,stderr.
+    with subprocess.Popen(
+            cmd_list,
+            shell=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT
+            ) as proc:
+        logging.info(proc.stdout.read())
+
+logging.info("All done.")
